37ec6f7796f26b98b5bb0fc605b856385b3facf1
diego211002 <142953161+diego211002@users.noreply.github.com>
2024-08-17 21:32:48 -0600

    Tarea actualizada 4.0 COMPLETA

--- Tareas/Tarea1.py
+++ Tareas/Tarea1.py
@@ -1,14 +1,13 @@
 import pandas as pd
-from statistics import mode
+from statistics import multimode, StatisticsError
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 # Leer  archivo CSV
 C6H6GT_data = pd.read_csv("AirQualityUCI.csv", delimiter=';')
 
-n = 9357  # cantidad de datos de la columna correspondiente del archivo
 
 #  Extrae la columna C6H6(GT) del archivo
 datos = C6H6GT_data['C6H6(GT)']
@@ -20,10 +19,15 @@ strDatos = datos.str.replace(',', '.')
 # elimina las filas que no tengan un valor numérico
 float_datos = strDatos.astype(float).dropna()
 
+# obtiene los datos que son mayores a cero
+float_datos_filtrados = float_datos[float_datos > 0]
+
+n = len(float_datos_filtrados)  # cantidad de datos de la columna
+
 
 #  Funcion para calcular promedio de datos
 def promedio_de_datos():
-    promedio = sum(float_datos)/n  # Calcula el promedio
+    promedio = sum(float_datos_filtrados)/n  # Calcula el promedio
     # promedio = datos.mean()
     return (promedio)
 
@@ -35,8 +39,8 @@ print("Promedio: ", promedio)
 
 # Funcion para calcular el rango de la muestra con minimo y maximo
 def rango_de_datos(C6H6GT_data):
-    minimo = min(float_datos)
-    maximo = max(float_datos)
+    minimo = min(float_datos_filtrados)
+    maximo = max(float_datos_filtrados)
     rango_muestral = maximo-minimo  # Formula de rango
     print("Rango: ", rango_muestral)
 
@@ -44,31 +48,48 @@ def rango_de_datos(C6H6GT_data):
 rango_de_datos(C6H6GT_data)
 
 
-# Funcion para encontrar la moda de la muestra de datos
 def moda_de_datos(C6H6GT_data):
-    moda = mode(float_datos)  # Libreria stadistics para encontrar Moda
-    print("Moda: ", moda)
+    # Verifica que haya datos suficientes después de filtrar
+    if len(float_datos_filtrados) == 0:
+        print("No hay datos no negativos en la muestra.")
+        return None
+
+    # Imprime los datos filtrados para ver qué valores se están procesando
+    print("Datos filtrados:", float_datos_filtrados)
+
+    try:
+        # Verifica si hay más de una moda usando multimode
+        modas = multimode(float_datos_filtrados)
+        if len(modas) == 1:
+            print("Moda: ", modas[0])
+        else:
+            print("Múltiples modas: ", modas)
+    except StatisticsError:
+        print("No se puede encontrar una moda única o los datos están vacíos.")
 
 
+# Llama a la función
 moda_de_datos(C6H6GT_data)
 
 
 # Función para calcular la mediana de la muestra de datos
 def mediana_de_datos(C6H6GT_data):
-    mediana = np.median(float_datos)  # Libreria numpy para encontrar mediana
+    # Libreria numpy para encontrar mediana
+    mediana = np.median(float_datos_filtrados)
     print("Mediana: ", mediana)
 
 
 # Función para calcular los cuartiles y el rango intercuartilico (RIC)
+# Función para calcular los cuartiles y el rango intercuartilico (RIC)
 def cuartiles_de_datos(C6H6GT_data):
-    Q1 = np.percentile(float_datos, 25)  # Libreria numpy para encontrar Q1
-    Q2 = np.percentile(float_datos, 50)  # Este es igual a la mediana
-    Q3 = np.percentile(float_datos, 75)  # Libreria numpy para encontrar Q3
+    # Libreria numpy para encontrar Q1
+    Q1 = np.percentile(float_datos_filtrados, 25)
+    # Este es igual a la mediana
+    Q2 = np.percentile(float_datos_filtrados, 50)
+    # Libreria numpy para encontrar Q3
+    Q3 = np.percentile(float_datos_filtrados, 75)
     IQR = Q3 - Q1  # Formula de rango intercuartilico
-    print("Primer cuartil :", Q1)
-    print("Mediana :", Q2)
-    print("Tercer cuartil :", Q3)
-    print("Rango Intercuartílico :", IQR)
+    return Q1, Q2, Q3, IQR
 
 
 cuartiles_de_datos(C6H6GT_data)
@@ -76,11 +97,13 @@ cuartiles_de_datos(C6H6GT_data)
 
 #  Funcion que calcula la varianza de los datos
 def varianza_datos():
-    s_2 = sum((float_datos - promedio)**2)/n-1  # Formula de varianza
+    # Libreria Pandas para encontrar la varianza
+    s_2 = float_datos_filtrados.var()
     return s_2
 
 
 varianza = varianza_datos()
+print("Varianza :", varianza)
 
 
 #  Funcion que calcula la desviacion estandar de los datos
@@ -90,6 +113,7 @@ def desviacion_estandar_datos():
 
 
 desviacion_estandar = desviacion_estandar_datos()
+print("Desviacion estandar :", desviacion_estandar)
 
 
 #  Funcion que calcula el coeficiente de variacion de datos
@@ -103,16 +127,113 @@ print("C.V: ", coeficiente_variacion)
 
 
 # Diagrama de Cajas
+cuar1, cuar2, cuar3, rango_intercuartil = cuartiles_de_datos(C6H6GT_data)
 
-# Convierte los datos a una lista utilizando libreria Seaborn
-float_datos_list = float_datos.tolist()
 
-# Creacion de diagrama de cajas en forma horizontal
-sns.boxplot(data=float_datos_list, orient='h')
+# Calcula limite inferior y superior de los bigotes
+# del diagrama de cajas (distancia)
+lim_inferior = cuar1 - 1.5 * rango_intercuartil
+lim_superior = cuar3 + 1.5 * rango_intercuartil
 
-# Añadir título y etiquetas
-plt.title("Diagrama de Cajas para Datos de C6H6(GT)")
-plt.xlabel("Concentración de C6H6(GT)")
 
-# Se muestra el grafico como Figura 1
+# Identifica valores atípicos basados en los limites
+def encontrar_valores_atipicos():
+    valores_atipicos = []
+
+    for i in float_datos_filtrados:
+        if i < lim_inferior or i > lim_superior:
+            valores_atipicos.append(i)
+
+    return valores_atipicos
+
+
+valor_atipico = encontrar_valores_atipicos()
+
+
+# Identifica valores que se encuentran dentro de los limites
+def encontrar_datos_dentro_limites():
+    valores_dentro_limites = []
+    for i in float_datos_filtrados:
+        if lim_inferior <= i <= lim_superior:
+            valores_dentro_limites.append(i)
+    return valores_dentro_limites
+
+
+valores_dentro_lim = encontrar_datos_dentro_limites()
+
+
+min_dentro_lim = min(valores_dentro_lim)
+max_dentro_lim = max(valores_dentro_lim)
+
+
+fig, ax = plt.subplots()  # crea el gráfico y los ejes
+
+
+# Muestra el titulo y las etiquetas
+ax.set_title('Concentración real de benceno promediada por hora en microg/m^3')
+ax.set_xlabel('microg/m^3')
+
+
+# Oculta el eje y de la grafica
+ax.get_yaxis().set_visible(False)
+
+
+# Dibuja la caja desde el cuartil 1 al cuartil 3
+ax.add_patch(plt.Rectangle((cuar1, 0.75), rango_intercuartil, 0.5, fill=True, color='aquamarine'))
+
+
+# Dibuja la linea de la mediana
+ax.plot([cuar2, cuar2], [0.75, 1.25], color='deeppink', lw=1)
+
+
+# Dibjua los bigotes desde el cuartil 1
+# hasta el minimo y del cuartil 3 hasta el maximo
+ax.plot([min_dentro_lim, cuar1], [1, 1], color='black', lw=1)
+ax.plot([cuar3, max_dentro_lim], [1, 1], color='black', lw=1)
+
+
+# Dibuja las lineas verticales de los extremos de los bigotes
+ax.plot([min_dentro_lim, min_dentro_lim], [0.8, 1.2], color='black', lw=1)
+ax.plot([max_dentro_lim, max_dentro_lim], [0.8, 1.2], color='black', lw=1)
+
+
+# Dibuja los datos atipicos como puntos
+ax.scatter(valor_atipico, [1]*len(valor_atipico), color='deepskyblue')
+
+
+plt.show()
+
+
+# Histograma
+
+def numero_de_clases(n):
+    bins_sqrt = int(np.ceil(np.sqrt(n)))
+
+    return bins_sqrt
+
+
+# Calcular el número de bins usando diferentes reglas
+bins_sqrt = numero_de_clases(n)
+
+
+# Paso 1: Calcular el intervalo de los bins
+min_val = min(float_datos_filtrados)
+max_val = max(float_datos_filtrados)
+intervalo_bin = (max_val - min_val) / bins_sqrt
+
+# Paso 2: Crear los bordes de los bins
+bins = np.arange(min_val, max_val + intervalo_bin, intervalo_bin)
+
+# Paso 3: Calcular la frecuencia absoluta
+frecuencia_absoluta, _ = np.histogram(float_datos_filtrados, bins=bins)
+
+# Paso 4: Calcular la frecuencia relativa
+frecuencia_relativa = frecuencia_absoluta / len(float_datos_filtrados)
+
+# Paso 5: Crear el histograma de frecuencia absoluta
+plt.figure(figsize=(10, 6))
+plt.hist(float_datos_filtrados, bins=bins, color='skyblue', edgecolor='black')
+plt.title('Histograma de Concentración de Benceno (C6H6) en microg/m^3')
+plt.xlabel('Concentración de Benceno (microg/m^3)')
+plt.ylabel('Frecuencia Absoluta')
 plt.show()
